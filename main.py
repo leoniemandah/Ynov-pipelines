2f3ab9396bb36f6ee4a87f8b1d853379c4dc4b43
unknown <andjibyleonieexode.mandah@ynov.com>
2023-01-23 14:22:55 +0100

    pipelines5

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from bottle import route, run, template
 from misc import add
 
 
-@route('/hello/<name>')
+@route("/hello/<name>")
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return template("<b>Hello {{name}}</b>!", name=name)
 
 
-@route('/add/<n>/<m>')
-@route('/add/<n>/<m>/')
+@route("/add/<n>/<m>")
+@route("/add/<n>/<m>/")
 def route_add(n, m):
-    return {'result': add(n, m)}
+    return {"result": add(n, m)}
 
 
-run(host='localhost', port=8080)
+run(host="localhost", port=8080)
